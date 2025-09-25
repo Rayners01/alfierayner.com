af77e79dd484198e6df141944d5621ec781ad66c
Alfie Rayner <alfie7rayner@gmail.com>
2025-09-25 23:52:25 +0100

    literally one character ;(

--- src/pages/api/getToken.ts
+++ src/pages/api/getToken.ts
@@ -16,7 +16,7 @@ const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 
 const client_id = process.env.SPOTIFY_CLIENT_ID!;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET!;
-const redirect_uri = 'http://www.alfierayner.com/api/callback'; // must match your Spotify app settings exactly
+const redirect_uri = 'https://www.alfierayner.com/api/callback'; // must match your Spotify app settings exactly
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 
