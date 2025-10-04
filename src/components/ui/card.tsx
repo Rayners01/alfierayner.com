dcf992386b114805b465ba9d37199b437223ccaa
Alfie Rayner <alfie7rayner@gmail.com>
2025-10-04 21:53:42 +0100

    just testing deployment

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ export default function Card({
     <div
       className={`
         ${className}
-        bg-lime-200 border-green-700 border-2 rounded-lg
+        bg-orange-200 border-green-700 border-2 rounded-lg
         p-4 hover:border-yellow-400
       `}
       {...props}
