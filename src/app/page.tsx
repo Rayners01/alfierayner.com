8fb64835f7631614b97fed505d158f7dbd605773
Alfie Rayner <alfie7rayner@gmail.com>
2025-12-19 12:41:35 +0000

    added force-dynamic to ensure images are correct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ import PolaroidTransition from '@/components/pages/photos/transition';
 import PolaroidDeveloping from '@/components/pages/photos/developing';
 import Clock from '@/components/ui/clock';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
 
   const [offset, setOffset] = useState({ x: 0, y: 0 })
